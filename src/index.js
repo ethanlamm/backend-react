43a8eb067d3e4f544f3104bdf50048cb32ce962a
Ethan <1028716801@qq.com>
2022-10-02 12:44:57 +0800

    antd样式bug修复

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// 先引入antd样式
-import 'antd/dist/antd.css'
+// 先引入antd样式(修复bug，引入antd/dist/antd.min.css)
+// 官方：import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
 // 后引入公共样式，防止样式覆盖
 import './index.scss';
 // 根组件
