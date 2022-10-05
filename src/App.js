a3bbfac667e9fcdc523d1759a9ed0927c0c23a51
Ethan <1028716801@qq.com>
2022-10-05 10:00:39 +0800

    路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import './App.scss'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
-import AuthRoute from '@/components/AuthRoute';
-import GeekLayout from '@/pages/Layout';
-import Login from '@/pages/Login'
-import Home from './pages/Home';
-import Article from './pages/Article';
-import Publish from './pages/Publish';
+// 路由懒加载必要api和组件
+import { lazy, Suspense } from 'react'
+// 按需导入路由
+const AuthRoute=lazy(()=>import('./components/AuthRoute'))
+const GeekLayout = lazy(() => import('./pages/Layout'))
+const Login = lazy(() => import('./pages/Login'))
+const Home = lazy(() => import('./pages/Home'))
+const Article = lazy(() => import('./pages/Article'))
+const Publish = lazy(() => import('./pages/Publish'))
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          {/* 包裹需要鉴权的路由 */}
-          <Route path='/' element={<AuthRoute><GeekLayout /></AuthRoute>}>
-            {/* GeekLayout 下的二级路由 */}
-            <Route index element={<Home />}></Route>
-            <Route path='article' element={<Article />}></Route>
-            <Route path='publish' element={<Publish />}></Route>
-          </Route>
-          <Route path='/login' element={<Login />}></Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 200
+              }}
+            >
+              loading...
+            </div>
+          }
+        >
+          <Routes>
+            {/* 包裹需要鉴权的路由 */}
+            <Route path='/' element={<AuthRoute><GeekLayout /></AuthRoute>}>
+              {/* GeekLayout 下的二级路由 */}
+              <Route index element={<Home />}></Route>
+              <Route path='article' element={<Article />}></Route>
+              <Route path='publish' element={<Publish />}></Route>
+            </Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
